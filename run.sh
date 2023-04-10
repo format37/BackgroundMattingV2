ea32ad9c5502cf71dfa41c07e9a584584aafd0bc
alex <format37@gmail.com>
2023-04-10 23:38:07 +0400

    modified:   run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 # clear
 # run with name bgmatting
-sudo docker run -it --rm -v /home/alex/projects/BackgroundMattingV2:/app --gpus all --name bgmatting bgmatting
+# sudo docker run -it --rm -v /home/alex/projects/BackgroundMattingV2:/app --gpus all --name bgmatting bgmatting
+sudo docker run -it --rm -v /home/alex/projects/BackgroundMattingV2:/app --gpus '"device=1"' --name bgmatting bgmatting
